d1cb3a0d4f19f62cd4b3cf130753560e0c837aca
JanaAdel1010 <janaadel2000@gmail.com>
2024-12-14 19:27:58 +0200

    Data split+ Ensemble Methods

---
+++ Bagging.py
@@ -0,0 +1,22 @@
+import numpy as np
+class Bagging:
+    def __init__(self, model, n):
+        self.model = model
+        self.n = n
+        self.models = []
+
+    def fit(self, X, y):
+        self.models = [] 
+        for _ in range(self.n):
+            indices = np.random.choice(range(len(X)), size=len(X), replace=True)
+            X_sample = X[indices]
+            y_sample = y[indices]
+
+            model = self.model()
+            model.fit(X_sample, y_sample)
+            self.models.append(model)
+
+    def predict(self, X):
+        predictions = np.array([model.predict(X) for model in self.models])
+        majority_votes = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=predictions)
+        return majority_votes
---
+++ Boosting.py
@@ -0,0 +1,37 @@
+import numpy as np
+class Boosting:
+    def __init__(self, model, n):
+        self.model = model
+        self.n = n
+        self.models = []
+        self.model_weights = []
+
+    def fit(self, X, y):
+        self.models = []  
+        self.model_weights = []  
+        sample_weights = np.ones(len(y)) / len(y)  
+
+        for _ in range(self.n):
+            model = self.model()
+            model.fit(X, y, sample_weight=sample_weights)
+            predictions = model.predict(X)
+
+            error = np.sum(sample_weights * (predictions != y)) / np.sum(sample_weights)
+
+            if error == 0:
+                self.models.append(model)
+                self.model_weights.append(1)
+                break
+
+            model_weight = 0.5 * np.log((1 - error) / error)
+            self.models.append(model)
+            self.model_weights.append(model_weight)
+
+            sample_weights = sample_weights * np.exp(-model_weight * y * predictions)
+            sample_weights /= np.sum(sample_weights)
+
+    def predict(self, X):
+        weighted_predictions = np.zeros(len(X))
+        for model, weight in zip(self.models, self.model_weights):
+            weighted_predictions += weight * model.predict(X)
+        return np.sign(weighted_predictions)
